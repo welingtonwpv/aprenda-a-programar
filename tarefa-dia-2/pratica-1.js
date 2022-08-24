31df7064ad9569d1a5e475b6dd4578b988d8355e
Welington Pereira <welingtonwpv@gmail.com>
2022-08-24 09:18:26 -0300

    Respostas ok

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,6 +1,17 @@
-let nome = "Welington Pereira"
-let Email = "welingtonwpv@gmail.com"
-let profissao = "Militar"
-let idade = 25
-let local "Los Angeles EUA"
-console.log("")
+let seunome
+seunome = "Welington Pereira"
+let seuemail 
+seuemail = "welingtonwpv@gmail.com"
+let suaprofissao 
+suaprofissao = "Militar"
+let suaidade
+suaidade = 25
+let seulocal 
+seulocal = "Los Angeles EUA"
+
+
+console.log(seunome)
+console.log(seuemail)
+console.log(suaprofissao)
+console.log(suaidade)
+console.log(seulocal)
