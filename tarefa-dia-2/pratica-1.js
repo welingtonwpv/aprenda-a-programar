5a1ca0eb8d218b2782c88fa0f79b8a6f843da14f
Welington Pereira <welingtonwpv@gmail.com>
2022-08-24 09:33:41 -0300

    Teste

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,17 +1,21 @@
 let seunome
 seunome = "Welington Pereira"
+
 let seuemail 
 seuemail = "welingtonwpv@gmail.com"
+
 let suaprofissao 
 suaprofissao = "Militar"
+
 let suaidade
 suaidade = 25
+
 let seulocal 
 seulocal = "Los Angeles EUA"
-
 
 console.log(seunome)
 console.log(seuemail)
 console.log(suaprofissao)
 console.log(suaidade)
 console.log(seulocal)
+
