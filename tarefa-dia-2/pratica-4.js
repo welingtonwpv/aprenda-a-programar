e3b5e8ca167aff67d998fe1b6adbd3ea7ea8168e
Welington Pereira <welingtonwpv@gmail.com>
2022-08-24 14:39:40 -0300

    headme

--- tarefa-dia-2/pratica-4.js
+++ tarefa-dia-2/pratica-4.js
@@ -3,3 +3,5 @@ let produtoB = {"nome": "perfume", "valor": 200.00, "internacional": true};
 let produtoC = {"nome": "sandália", "valor": 120.00, "internacional": false};
 
 console.log(produtoA)
+console.log(produtoB)
+console.log(produtoC)
