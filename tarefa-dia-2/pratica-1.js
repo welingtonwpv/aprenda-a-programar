0a6bc16e4eb75a1113a50e4f5e2d5c7b37400031
Welington Pereira <welingtonwpv@gmail.com>
2022-08-24 14:12:09 -0300

    variavel no texto

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -19,3 +19,4 @@ console.log(suaprofissao)
 console.log(suaidade)
 console.log(seulocal)
 
+console.log('Olá, ' + seunome + '! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ' + seulocal + '. Confirma pra gente alguns dados? A sua idade é <valor da variável que armazena sua idade>, você é <valor da variável que armazena sua profissão> e o seu email é <valor da variável que armazena seu email>? Ficamos no aguardo. Muito obrigado e boa viagem! :D')
