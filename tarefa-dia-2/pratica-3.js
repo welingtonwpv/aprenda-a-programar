89c73a4cd692e174b3da7d436942fcb0ff16f50b
Welington Pereira <welingtonwpv@gmail.com>
2022-08-24 09:58:32 -0300

    iagual a

--- tarefa-dia-2/pratica-3.js
+++ tarefa-dia-2/pratica-3.js
@@ -2,3 +2,4 @@ var stringExemplo = "ABCDBDHEKSJLFYHAZDNJCLAAWJBHTZVEKVMIFKHFKCEHEKLUTBQLFYHAZDN
 var tamanho = stringExemplo.length;
 
 console.log(tamanho)
+console.log("Maravilha! O código tem menos ou é igual a 6111 caracteres! Mãos à obra! Decifrem o código o quanto antes que vocês vão conseguir derrotá-los! :D")
