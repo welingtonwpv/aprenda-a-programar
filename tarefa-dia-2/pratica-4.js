b2453da8c848bd1c3e2c459189c77def4dad01fb
Welington Pereira <welingtonwpv@gmail.com>
2022-08-25 08:43:22 -0300

    pratica 4

--- tarefa-dia-2/pratica-4.js
+++ tarefa-dia-2/pratica-4.js
@@ -2,6 +2,23 @@ let produtoA = {"nome": "camiseta", "valor": 100.00, "internacional": true};
 let produtoB = {"nome": "perfume", "valor": 200.00, "internacional": true};
 let produtoC = {"nome": "sandália", "valor": 120.00, "internacional": false};
 
-console.log(produtoA)
-console.log(produtoB)
-console.log(produtoC)
+let produto1 = "camiseta"
+let valor1 = 100.00
+let nacionalidade1 = "internacional"
+let camisetaImposto = 100 * 1.2
+
+let produto2 = "camiseta"
+let valor2 = 100.00
+let nacionalidade2 = "internacional"
+let perfumeImposto = 200 * 1.2
+
+let produto3 = "camiseta"
+let valor3 = 100.00
+let nacionalidade3 = "internacional"
+let sandaliaImposto = 120 * 1.2
+
+console.log('Produto numero 1: '+produto1+', valor: '+valor1+', nacionalidade: '+nacionalidade1+', total com imposto: '+camisetaImposto+'')
+
+console.log('Produto numero 2: '+produto2+', valor: '+valor2+', nacionalidade: '+nacionalidade2+', total com imposto: '+perfumeImposto+'')
+
+console.log('Produto numero 3: '+produto3+', valor: '+valor3+', nacionalidade: '+nacionalidade3+', total com imposto: '+sandaliaImposto+'')
