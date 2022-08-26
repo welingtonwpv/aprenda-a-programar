082ccdebb1e9c95e43fb7fa25a639b57279e65a6
Welington Pereira <welingtonwpv@gmail.com>
2022-08-26 08:32:04 -0300

    parte 3 ok

--- tarefa-dia-3/pratica-3.js
+++ tarefa-dia-3/pratica-3.js
@@ -1,10 +1,12 @@
 var j=0, msg="";
 while (j<=16){
      
+     //só concatena se forem números impares
      if(j%2 !=0){
         msg += j + ", ";
      }
     j++;
   };
+//retira ultima virgula com ultimo espaço 
 msg = msg.substr(0,(msg.length -2));
 console.log(msg);
